23128dc8750d7562f1c9dffbb95d86470a302dbb
Poulami <poulami.0709@gmail.com>
2025-07-19 15:04:36 +0530

    Added unit tests and test.yml

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,1 +1,31 @@
  
+import json
+import os
+import pytest
+from sklearn.linear_model import LogisticRegression
+from src.train import load_config, train_model
+from sklearn.datasets import load_digits
+from sklearn.metrics import accuracy_score
+
+def test_config_loading():
+    config = load_config("config/config.json")
+    assert "C" in config and isinstance(config["C"], float)
+    assert "solver" in config and isinstance(config["solver"], str)
+    assert "max_iter" in config and isinstance(config["max_iter"], int)
+
+def test_model_training():
+    config = load_config("config/config.json")
+    digits = load_digits()
+    X, y = digits.data, digits.target
+    model = train_model(X, y, config)
+    assert isinstance(model, LogisticRegression)
+    assert hasattr(model, "coef_")
+
+def test_model_accuracy():
+    config = load_config("config/config.json")
+    digits = load_digits()
+    X, y = digits.data, digits.target
+    model = train_model(X, y, config)
+    preds = model.predict(X)
+    acc = accuracy_score(y, preds)
+    assert acc > 0.9  # Check if training worked
